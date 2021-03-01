34bf009901ee7bb1b5104e8fe116c71184ab70a0
Uday Garg <u.garg14@gmail.com>
2021-03-01 16:46:52 +0530

    implemented emi calculator and pie chart

---
+++ app/src/main/java/com/example/caremicalculator/MainActivity.java
@@ -0,0 +1,141 @@
+package com.example.caremicalculator;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ConstraintLayout constraintLayout;
+    SeekBar interestSeekBar;
+    SeekBar tenureSeekBar;
+    float interestRate = 1;
+    float tenureRate = 1;
+    TextView emiTextView;
+    float emi;
+    EditText loanEditText;
+    TextView interestTextView;
+    TextView tenureTextView;
+    float interestAmount;
+    float totalPayment;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        setTitle("");
+        constraintLayout = findViewById(R.id.constraintLayout);
+        constraintLayout.setOnClickListener(this);
+        interestSeekBar = findViewById(R.id.interestRateSeekBar);
+        tenureSeekBar = findViewById(R.id.tenureSeekBar);
+//        emiTextView = findViewById(R.id.emiTextView);
+        interestTextView = findViewById(R.id.interestTextView);
+        tenureTextView = findViewById(R.id.tenureTextView);
+//        emiTextView.setVisibility(View.INVISIBLE);
+        loanEditText = findViewById(R.id.loanEditText);
+        interestTextView.setText("1");
+        tenureTextView.setText("1");
+        interestSeekBar.setMax(20);
+        interestSeekBar.setProgress(1);
+
+        interestSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                interestRate = (float) i;
+                if (i < 1) {
+                    seekBar.setProgress(1);
+                }
+                if (i != 0) {
+                    interestTextView.setText(String.valueOf(i));
+
+                }
+                Log.i("seekbar", String.valueOf(interestRate));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        tenureSeekBar.setMax(7);
+        tenureSeekBar.setProgress(1);
+        tenureSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                tenureRate = (float) i;
+                if (i < 1) {
+                    seekBar.setProgress(1);
+
+                }
+                if (i != 0) {
+                    tenureTextView.setText(String.valueOf(i));
+                }
+                Log.i("seekbar", String.valueOf(tenureRate));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.constraintLayout) {
+            InputMethodManager im = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+            im.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+
+    }
+
+    public void calculateEMI(View view) {
+        if (loanEditText.getText().length() > 0) {
+
+
+            float newTenure = tenureRate * 12;
+            emi = (float) ((float) (Float.parseFloat(loanEditText.getText().toString()) * interestRate / 12 / 100 * Math.pow(1 + interestRate / 12 / 100, newTenure)) / (Math.pow(1 + interestRate / 12 / 100, newTenure) - 1));
+
+
+            interestAmount = -(Float.parseFloat(loanEditText.getText().toString()) - (emi * 12 * tenureRate));
+            totalPayment = interestAmount + Float.parseFloat(loanEditText.getText().toString());
+
+            Intent intent = new Intent(this, ResultActivity.class);
+            intent.putExtra("emi", emi);
+            intent.putExtra("interestAmount", interestAmount);
+            intent.putExtra("totalAmount", totalPayment);
+            intent.putExtra("principalAmount", Integer.parseInt(loanEditText.getText().toString()));
+            startActivity(intent);
+
+        } else {
+            Toast.makeText(this, "Please enter Loan Amount", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/caremicalculator/ResultActivity.java
@@ -0,0 +1,96 @@
+package com.example.caremicalculator;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class ResultActivity extends AppCompatActivity {
+    TextView emiTextView, interestAmountTextView, totalPaymentTextView;
+
+    PieChart pieChart;
+    PieData pieData;
+    PieDataSet pieDataSet;
+    ArrayList pieEntries;
+    ArrayList PieEntryLabels;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        Intent intent = getIntent();
+
+
+        emiTextView = findViewById(R.id.emiTextView);
+        interestAmountTextView = findViewById(R.id.interestAmountTextView);
+        totalPaymentTextView = findViewById(R.id.totalPaymentTextView);
+        Locale indialocale = new Locale("en", "IN");
+        NumberFormat india = NumberFormat.getCurrencyInstance(indialocale);
+
+        String emi = india.format((int) intent.getFloatExtra("emi", 0));
+        String[] emi1 = emi.split("\\.");
+
+        String interest = india.format((int) intent.getFloatExtra("interestAmount", 0));
+        String[] interest1 = interest.split("\\.");
+        String totalPayment = india.format((int) intent.getFloatExtra("totalAmount", 0));
+        String[] totalPayment1 = totalPayment.split("\\.");
+
+//
+//        pieChart.addPieSlice(
+//                new PieModel(
+//                        String.valueOf((int) intent.getFloatExtra("interestAmount", 0)),
+//                        Integer.parseInt(String.valueOf((int) intent.getFloatExtra("interestAmount", 0))),
+//                        Color.parseColor("#66BB6A")));
+//        pieChart.addPieSlice(
+//                new PieModel(
+//                        String.valueOf(intent.getFloatExtra("principalAmount", 0)),
+//                        Integer.parseInt(String.valueOf((int) intent.getIntExtra("principalAmount", 0))),
+//                        Color.parseColor("#EF5350")));
+//
+//
+//
+//        pieChart.setInnerValueString(String.valueOf(intent.getFloatExtra("totalAmount", 0)));
+//
+//
+//        pieChart.startAnimation();
+        pieChart = findViewById(R.id.pieChart);
+        pieEntries = new ArrayList<>();
+        pieEntries.add(new PieEntry((int) intent.getFloatExtra("interestAmount", 0), Integer.parseInt(String.valueOf((int) intent.getFloatExtra("interestAmount", 0)))));
+        pieEntries.add(new PieEntry((int) intent.getIntExtra("principalAmount", 0), Integer.parseInt(String.valueOf((int) intent.getIntExtra("principalAmount", 0)))));
+
+        pieDataSet = new PieDataSet(pieEntries, "");
+        pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        pieDataSet.setSliceSpace(2f);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(10f);
+        pieDataSet.setSliceSpace(5f);
+        pieChart.getDescription().setText("LOAN PAYMENT CHART");
+        pieChart.getDescription().setTextColor(Color.YELLOW);
+        pieChart.getDescription().setTextSize(15);
+        pieChart.setCenterText(totalPayment1[0]);
+
+
+
+        pieChart.animate();
+
+        emiTextView.setText(emi1[0]);
+        interestAmountTextView.setText(interest1[0]);
+        totalPaymentTextView.setText(totalPayment1[0]);
+    }
+
+}
